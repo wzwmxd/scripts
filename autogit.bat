f6b1512ff6f3fc4d8f92204756468293b1135114
wzwmxd <wzwmxd@qq.com>
2016-08-24 00:45:46 +0800

    20160824206024

--- autogit.bat
+++ autogit.bat
@@ -1,31 +1,37 @@
-@echo off
+
 set %path%=%path%;C:\Program Files\Git\bin\
 cd C:\C\article
 git add .
-git commit -m 'auto_commit'
+set time=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+git commit -m %time%
 git push origin master
 
 cd C:\C\C
 git add .
-git commit -m 'auto_commit'
+set time=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+git commit -m %time%
 git push origin master
 
 cd C:\C\docs
 git add .
-git commit -m 'auto_commit'
+set time=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+git commit -m %time%
 git push origin master
 
 cd C:\C\mcm
 git add .
-git commit -m 'auto_commit'
+set time=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+git commit -m %time%
 git push origin master
 
 cd C:\C\scripts
 git add .
-git commit -m 'auto_commit'
+set time=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+git commit -m %time%
 git push origin master
 
 cd C:\C\tips
 git add .
-git commit -m 'auto_commit'
+set time=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+git commit -m %time%
 git push origin master
