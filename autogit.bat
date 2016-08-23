5a6aecab765e881c9bf3bee644bac607f421cdac
wzwmxd <wzwmxd@qq.com>
2016-08-24 00:39:41 +0800

    'auto_commit'

--- autogit.bat
+++ autogit.bat
@@ -1,4 +1,30 @@
 set %path%=%path%;C:\Program Files\Git\bin\
+cd C:\C\article
+git add .
+git commit -m 'auto_commit'
+git push origin master
+
+cd C:\C\C
+git add .
+git commit -m 'auto_commit'
+git push origin master
+
+cd C:\C\docs
+git add .
+git commit -m 'auto_commit'
+git push origin master
+
+cd C:\C\mcm
+git add .
+git commit -m 'auto_commit'
+git push origin master
+
+cd C:\C\scripts
+git add .
+git commit -m 'auto_commit'
+git push origin master
+
+cd C:\C\tips
 git add .
 git commit -m 'auto_commit'
 git push origin master
